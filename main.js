e6173eb41c2c5279ff67d089b6ac570a08d07d99
José Protásio <joseprotasioss@gmail.com>
2021-11-12 12:30:26 -0300

    adicionamos o botão zerar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let add = document.getElementById("increment");
 let remove = document.getElementById("decrement");
-
+let zerar = document.getElementById("zerar");
 let int = document.getElementById("number");
 let integer = 0;
 
@@ -12,6 +12,11 @@ add.addEventListener("click", function(){
 remove.addEventListener("click", function(){
     integer -= 1;
     int.innerHTML = integer;
+})
+
+zerar.addEventListener("click", function(){
+integer = 0
+int.innerHTML = contador = 0;
 })
 
  
