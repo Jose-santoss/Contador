2f917ebb1c7035eac84a7bdcc8c48bbda3ab6908
José Protásio <joseprotasioss@gmail.com>
2022-02-02 14:21:50 -0300

    .

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ let int = document.getElementById("number");
 let integer = 0;
 
 add.addEventListener("click", function(){
-    integer += 1;
-    int.innerHTML = integer;
+integer +=1;
+int.innerHTML= integer;
 })
 
 remove.addEventListener("click", function(){
-    integer -= 1;
-    int.innerHTML = integer;
-})
+    integer -=1;
+    int.innerHTML= integer;
+    })
 
-zerar.addEventListener("click", function(){
-integer = 0
-int.innerHTML = contador = 0;
-})
+    zerar.addEventListener("click", function(){
+        integer = 0;
+        int.innerHTML = contador = 0;
+        })
+
+
+
+
 
 
 
